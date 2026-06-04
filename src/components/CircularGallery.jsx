8fa898889244ff6dd52fc940e748ca0df9899248
muskan ali <muskanali@muskis-mb-pro-10.local>
2026-06-05 00:53:05 +0530

    Updated achievements section and CurvedLoop

--- src/components/CircularGallery.jsx
+++ src/components/CircularGallery.jsx
@@ -334,18 +334,12 @@ class App {
   }
   createMedias(items, bend = 1, textColor, borderRadius, font) {
     const defaultItems = [
-      { image: `https://picsum.photos/seed/1/800/600?grayscale`, text: 'Bridge' },
-      { image: `https://picsum.photos/seed/2/800/600?grayscale`, text: 'Desk Setup' },
-      { image: `https://picsum.photos/seed/3/800/600?grayscale`, text: 'Waterfall' },
-      { image: `https://picsum.photos/seed/4/800/600?grayscale`, text: 'Strawberries' },
-      { image: `https://picsum.photos/seed/5/800/600?grayscale`, text: 'Deep Diving' },
-      { image: `https://picsum.photos/seed/16/800/600?grayscale`, text: 'Train Track' },
-      { image: `https://picsum.photos/seed/17/800/600?grayscale`, text: 'Santorini' },
-      { image: `https://picsum.photos/seed/8/800/600?grayscale`, text: 'Blurry Lights' },
-      { image: `https://picsum.photos/seed/9/800/600?grayscale`, text: 'New York' },
-      { image: `https://picsum.photos/seed/10/800/600?grayscale`, text: 'Good Boy' },
-      { image: `https://picsum.photos/seed/21/800/600?grayscale`, text: 'Coastline' },
-      { image: `https://picsum.photos/seed/12/800/600?grayscale`, text: 'Palm Trees' }
+      { image: `muskan1`, text: 'Bridge' },
+      { image: `muskan2`, text: 'Desk Setup' },
+      { image: `muskan3`, text: 'Waterfall' },
+      { image: `muskan4`, text: 'Strawberries' },
+      { image: `muskan5`, text: 'Deep Diving' },
+      { image: `muskan6`, text: 'Train Track' }
     ];
     const galleryItems = items && items.length ? items : defaultItems;
     this.mediasImages = galleryItems.concat(galleryItems);
